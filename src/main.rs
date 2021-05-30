5cafb63e5510da163c6f78d5c5bb210dad18e3ec
Isaac Diamond <isaacd9@gmail.com>
2021-05-30 15:50:53 -0700

    minor refactor

--- src/main.rs
+++ src/main.rs
@@ -17,196 +17,207 @@ pub mod runtime {
     include!(concat!(env!("OUT_DIR"), "/runtime.v1alpha2.rs"));
 }
 
-async fn get_and_print_version(
-    client: &mut RuntimeServiceClient<tonic::transport::Channel>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    info!("Connecting to peer at {}", ADDR);
-
-    let version_req = VersionRequest {
-        version: String::from("0.0.1-unnamed-rust-cri-client"),
-    };
-    let request = tonic::Request::new(version_req);
-    let response = client.version(request).await?;
-
-    let msg = response.into_inner();
-
-    info!("Peer is {}@{}", msg.runtime_name, msg.runtime_version);
-    Ok(())
+struct PodManager {
+    runtime_service_client: RuntimeServiceClient<tonic::transport::Channel>,
+    image_service_client: ImageServiceClient<tonic::transport::Channel>,
 }
 
-async fn pod_exists(
-    name: &String,
-    client: &mut RuntimeServiceClient<tonic::transport::Channel>,
-) -> Result<bool, Box<dyn std::error::Error>> {
-    let request = ListPodSandboxRequest::default();
+impl PodManager {
+    pub async fn get_and_print_version(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        info!("Connecting to peer at {}", ADDR);
 
-    info!("Getting sandbox status {:?}", request);
-    let response = client.list_pod_sandbox(request).await?;
-    let msg = response.into_inner();
-    info!("peer responded {:?}", msg);
-    let exists = msg.items.into_iter().any(|pod_sandbox| {
-        pod_sandbox
-            .metadata
-            .filter(|metadata| metadata.name == *name)
-            .is_some()
-    });
-    Ok(exists)
-}
+        let version_req = VersionRequest {
+            version: String::from("0.0.1-unnamed-rust-cri-client"),
+        };
+        let request = tonic::Request::new(version_req);
+        let response = self.runtime_service_client.version(request).await?;
 
-fn pod_sandbox_config(name: &String, uid: &String) -> PodSandboxConfig {
-    let mut security_context = LinuxSandboxSecurityContext::default();
+        let msg = response.into_inner();
 
-    security_context.namespace_options = Some(NamespaceOption {
-        network: NamespaceMode::Node as i32,
-        pid: NamespaceMode::Container as i32,
-        ipc: NamespaceMode::Pod as i32,
-        target_id: String::from(""),
-    });
-
-    let mut pod_sandbox_config = PodSandboxConfig::default();
-    pod_sandbox_config.metadata = Some(PodSandboxMetadata {
-        name: String::from(name),
-        uid: uid.clone(),
-        namespace: String::from("my-test-name-namespace"),
-        attempt: 0,
-    });
-    pod_sandbox_config.linux = Some(LinuxPodSandboxConfig {
-        cgroup_parent: String::from(""),
-        security_context: Some(security_context),
-        sysctls: HashMap::default(),
-    });
-    pod_sandbox_config
-}
-
-async fn run_pod_sandbox(
-    pod_sandbox_config: &PodSandboxConfig,
-    client: &mut RuntimeServiceClient<tonic::transport::Channel>,
-) -> Result<String, Box<dyn std::error::Error>> {
-    let request = RunPodSandboxRequest {
-        config: Some(pod_sandbox_config.clone()),
-        runtime_handler: String::from(""),
-    };
-
-    info!("Creating pod sandbox {:?}", request);
-    let response = client.run_pod_sandbox(request).await?;
-    let msg = response.into_inner();
-    info!("peer responded {:?}", msg);
-    Ok(msg.pod_sandbox_id)
-}
-
-async fn create_containers(
-    image_id: &String,
-    pod_sandbox_id: &String,
-    pod_sandbox_config: &PodSandboxConfig,
-    client: &mut RuntimeServiceClient<tonic::transport::Channel>,
-) -> Result<String, Box<dyn std::error::Error>> {
-    let mut container_config = ContainerConfig::default();
-    container_config.metadata = Some(ContainerMetadata {
-        name: String::from("my-container"),
-        attempt: 0,
-    });
-    container_config.image = Some(ImageSpec {
-        image: image_id.clone(),
-        annotations: HashMap::default(),
-    });
-
-    let request = CreateContainerRequest {
-        pod_sandbox_id: pod_sandbox_id.clone(),
-        config: Some(container_config),
-        sandbox_config: Some(pod_sandbox_config.clone()),
-    };
-    info!("Creating container {:?}", request);
-    let response = client.create_container(request).await?;
-    let msg = response.into_inner();
-    info!("peer responded {:?}", msg);
-    Ok(msg.container_id)
-}
-
-async fn start_containers(
-    container_id: &String,
-    client: &mut RuntimeServiceClient<tonic::transport::Channel>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let request = StartContainerRequest {
-        container_id: container_id.clone(),
-    };
-    info!("Starting container {:?}", request);
-    let response = client.start_container(request).await?;
-    let msg = response.into_inner();
-    info!("peer responded {:?}", msg);
-    Ok(())
-}
-
-async fn poll_container_status(
-    container_id: &String,
-    client: &mut RuntimeServiceClient<tonic::transport::Channel>,
-) -> Result<ContainerState, Box<dyn std::error::Error>> {
-    let res = client
-        .container_status(ContainerStatusRequest {
-            container_id: container_id.clone(),
-            verbose: true,
-        })
-        .await?;
-    let msg = res.into_inner();
-    info!("{:?}", msg);
-
-    if let Some(status) = msg.status {
-        if let Some(state) = ContainerState::from_i32(status.state) {
-            return Ok(state);
-        }
+        info!("Peer is {}@{}", msg.runtime_name, msg.runtime_version);
+        Ok(())
     }
 
-    Ok(ContainerState::ContainerUnknown)
-}
+    pub async fn pod_exists(&mut self, name: &String) -> Result<bool, Box<dyn std::error::Error>> {
+        let request = ListPodSandboxRequest::default();
 
-async fn stop_pod(
-    pod_sandbox_id: &String,
-    client: &mut RuntimeServiceClient<tonic::transport::Channel>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let res = client
-        .stop_pod_sandbox(StopPodSandboxRequest {
-            pod_sandbox_id: pod_sandbox_id.clone(),
-        })
-        .await?;
-    let msg = res.into_inner();
-    info!("{:?}", msg);
+        info!("Getting sandbox status {:?}", request);
+        let response = self
+            .runtime_service_client
+            .list_pod_sandbox(request)
+            .await?;
+        let msg = response.into_inner();
+        info!("peer responded {:?}", msg);
+        let exists = msg.items.into_iter().any(|pod_sandbox| {
+            pod_sandbox
+                .metadata
+                .filter(|metadata| metadata.name == *name)
+                .is_some()
+        });
+        Ok(exists)
+    }
 
-    Ok(())
-}
+    fn pod_sandbox_config(&self, name: &String, uid: &String) -> PodSandboxConfig {
+        let mut security_context = LinuxSandboxSecurityContext::default();
 
-async fn destroy_pod(
-    pod_sandbox_id: &String,
-    client: &mut RuntimeServiceClient<tonic::transport::Channel>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let res = client
-        .remove_pod_sandbox(RemovePodSandboxRequest {
-            pod_sandbox_id: pod_sandbox_id.clone(),
-        })
-        .await?;
-    let msg = res.into_inner();
-    info!("{:?}", msg);
+        security_context.namespace_options = Some(NamespaceOption {
+            network: NamespaceMode::Node as i32,
+            pid: NamespaceMode::Container as i32,
+            ipc: NamespaceMode::Pod as i32,
+            target_id: String::from(""),
+        });
 
-    Ok(())
-}
+        let mut pod_sandbox_config = PodSandboxConfig::default();
+        pod_sandbox_config.metadata = Some(PodSandboxMetadata {
+            name: String::from(name),
+            uid: uid.clone(),
+            namespace: String::from("my-test-name-namespace"),
+            attempt: 0,
+        });
+        pod_sandbox_config.linux = Some(LinuxPodSandboxConfig {
+            cgroup_parent: String::from(""),
+            security_context: Some(security_context),
+            sysctls: HashMap::default(),
+        });
+        pod_sandbox_config
+    }
 
-async fn pull_image(
-    image_id: &String,
-    pod_sandbox_config: &PodSandboxConfig,
-    client: &mut ImageServiceClient<tonic::transport::Channel>,
-) -> Result<String, Box<dyn std::error::Error>> {
-    let request = PullImageRequest {
-        image: Some(ImageSpec {
+    pub async fn run_pod_sandbox(
+        &mut self,
+        pod_sandbox_config: &PodSandboxConfig,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let request = RunPodSandboxRequest {
+            config: Some(pod_sandbox_config.clone()),
+            runtime_handler: String::from(""),
+        };
+
+        info!("Creating pod sandbox {:?}", request);
+        let response = self.runtime_service_client.run_pod_sandbox(request).await?;
+        let msg = response.into_inner();
+        info!("peer responded {:?}", msg);
+        Ok(msg.pod_sandbox_id)
+    }
+
+    async fn create_containers(
+        &mut self,
+        image_id: &String,
+        pod_sandbox_id: &String,
+        pod_sandbox_config: &PodSandboxConfig,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let mut container_config = ContainerConfig::default();
+        container_config.metadata = Some(ContainerMetadata {
+            name: String::from("my-container"),
+            attempt: 0,
+        });
+        container_config.image = Some(ImageSpec {
             image: image_id.clone(),
             annotations: HashMap::default(),
-        }),
-        auth: None,
-        sandbox_config: Some(pod_sandbox_config.clone()),
-    };
+        });
 
-    info!("Pulling image {:?}", request);
-    let response = client.pull_image(request).await?;
-    let msg = response.into_inner();
-    info!("peer responded {:?}", msg);
-    Ok(msg.image_ref)
+        let request = CreateContainerRequest {
+            pod_sandbox_id: pod_sandbox_id.clone(),
+            config: Some(container_config),
+            sandbox_config: Some(pod_sandbox_config.clone()),
+        };
+        info!("Creating container {:?}", request);
+        let response = self
+            .runtime_service_client
+            .create_container(request)
+            .await?;
+        let msg = response.into_inner();
+        info!("peer responded {:?}", msg);
+        Ok(msg.container_id)
+    }
+
+    async fn start_containers(
+        &mut self,
+        container_id: &String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let request = StartContainerRequest {
+            container_id: container_id.clone(),
+        };
+        info!("Starting container {:?}", request);
+        let response = self.runtime_service_client.start_container(request).await?;
+        let msg = response.into_inner();
+        info!("peer responded {:?}", msg);
+        Ok(())
+    }
+
+    async fn poll_container_status(
+        &mut self,
+        container_id: &String,
+    ) -> Result<ContainerState, Box<dyn std::error::Error>> {
+        let res = self
+            .runtime_service_client
+            .container_status(ContainerStatusRequest {
+                container_id: container_id.clone(),
+                verbose: true,
+            })
+            .await?;
+        let msg = res.into_inner();
+        info!("{:?}", msg);
+
+        if let Some(status) = msg.status {
+            if let Some(state) = ContainerState::from_i32(status.state) {
+                return Ok(state);
+            }
+        }
+
+        Ok(ContainerState::ContainerUnknown)
+    }
+
+    async fn stop_pod(
+        &mut self,
+        pod_sandbox_id: &String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let res = self
+            .runtime_service_client
+            .stop_pod_sandbox(StopPodSandboxRequest {
+                pod_sandbox_id: pod_sandbox_id.clone(),
+            })
+            .await?;
+        let msg = res.into_inner();
+        info!("{:?}", msg);
+
+        Ok(())
+    }
+
+    async fn destroy_pod(
+        &mut self,
+        pod_sandbox_id: &String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let res = self
+            .runtime_service_client
+            .remove_pod_sandbox(RemovePodSandboxRequest {
+                pod_sandbox_id: pod_sandbox_id.clone(),
+            })
+            .await?;
+        let msg = res.into_inner();
+        info!("{:?}", msg);
+
+        Ok(())
+    }
+
+    async fn pull_image(
+        &mut self,
+        image_id: &String,
+        pod_sandbox_config: &PodSandboxConfig,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let request = PullImageRequest {
+            image: Some(ImageSpec {
+                image: image_id.clone(),
+                annotations: HashMap::default(),
+            }),
+            auth: None,
+            sandbox_config: Some(pod_sandbox_config.clone()),
+        };
+
+        info!("Pulling image {:?}", request);
+        let response = self.image_service_client.pull_image(request).await?;
+        let msg = response.into_inner();
+        info!("peer responded {:?}", msg);
+        Ok(msg.image_ref)
+    }
 }
 
 const ADDR: &'static str = "http://localhost:3000";
@@ -228,11 +239,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     signal_flag::register_usize(SIGINT, Arc::clone(&term), SIGINT_U)?;
     signal_flag::register_usize(SIGQUIT, Arc::clone(&term), SIGQUIT_U)?;
 
-    let mut runtime_service_client = RuntimeServiceClient::connect(ADDR).await?;
-    let mut image_service_client = ImageServiceClient::connect(ADDR).await?;
-    get_and_print_version(&mut runtime_service_client).await?;
+    let runtime_service_client = RuntimeServiceClient::connect(ADDR).await?;
+    let image_service_client = ImageServiceClient::connect(ADDR).await?;
 
-    if pod_exists(&String::from(NAME), &mut runtime_service_client).await? {
+    let mut m = PodManager {
+        runtime_service_client: runtime_service_client,
+        image_service_client: image_service_client,
+    };
+
+    m.get_and_print_version().await?;
+
+    if m.pod_exists(&String::from(NAME)).await? {
         let e = format!("the pod \"{}\" already exists", &String::from(NAME));
         return Err(e.into());
     }
@@ -243,29 +260,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //let config = read_yaml_config(stdin);
     //println!("{:?}", config);
 
-    let pod_sandbox_config = pod_sandbox_config(&String::from(NAME), &uid);
-    let pod_sandbox_id = run_pod_sandbox(&pod_sandbox_config, &mut runtime_service_client).await?;
-    let image_ref = pull_image(
-        &IMAGE.to_string(),
-        &pod_sandbox_config,
-        &mut image_service_client,
-    )
-    .await?;
-    let container = create_containers(
-        &IMAGE.to_string(),
-        &pod_sandbox_id,
-        &pod_sandbox_config,
-        &mut runtime_service_client,
-    )
-    .await?;
+    let pod_sandbox_config = m.pod_sandbox_config(&String::from(NAME), &uid);
+    let pod_sandbox_id = m.run_pod_sandbox(&pod_sandbox_config).await?;
+    let image_ref = m
+        .pull_image(&IMAGE.to_string(), &pod_sandbox_config)
+        .await?;
+    let container = m
+        .create_containers(&IMAGE.to_string(), &pod_sandbox_id, &pod_sandbox_config)
+        .await?;
 
-    start_containers(&container, &mut runtime_service_client).await?;
+    m.start_containers(&container).await?;
 
     loop {
         match term.load(Ordering::Relaxed) {
             0 => {
-                let status = poll_container_status(&container, &mut runtime_service_client).await?;
-                match status {
+                match m.poll_container_status(&container).await? {
                     ContainerState::ContainerExited => {
                         info!("container exited");
                         break;
@@ -291,8 +300,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     info!("cleaning up");
-    stop_pod(&pod_sandbox_id, &mut runtime_service_client).await?;
-    destroy_pod(&pod_sandbox_id, &mut runtime_service_client).await?;
+    m.stop_pod(&pod_sandbox_id).await?;
+    m.destroy_pod(&pod_sandbox_id).await?;
 
     info!("cleaned up");
     Ok(())
