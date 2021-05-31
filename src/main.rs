0a5c28cb53a5eca7f2e7cb80846b24e8502d5994
Isaac Diamond <isaacd9@gmail.com>
2021-05-30 17:07:15 -0700

    wire up to cfg file

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,6 @@ pub mod runtime {
 }
 
 const ADDR: &'static str = "http://localhost:3000";
-const IMAGE: &'static str = "gcr.io/google_containers/pause-amd64:3.0";
-const NAME: &'static str = "a nice pod 2";
-const CONTAINER_NAME: &'static str = "test-container";
 
 struct PodManager {
     runtime_service_client: RuntimeServiceClient<tonic::transport::Channel>,
@@ -231,7 +228,7 @@ impl PodManager {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config_file = env::args().nth(1).unwrap();
-    let pod = read_yaml_config(File::open(config_file)?);
+    let pod = read_yaml_config(File::open(config_file)?)?;
 
     simple_logger::SimpleLogger::new()
         .with_level(log::LevelFilter::Info)
@@ -258,41 +255,50 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     m.get_and_print_version().await?;
 
-    if m.pod_exists(&String::from(NAME)).await? {
-        let e = format!("the pod \"{}\" already exists", &String::from(NAME));
+    if m.pod_exists(&pod.name).await? {
+        let e = format!("the pod \"{}\" already exists", &pod.name);
         return Err(e.into());
     }
 
     let mut rng = rand::thread_rng();
-    let uid = format!("{}-{}", NAME, rng.gen::<u128>());
+    let uid = format!("{}-{}", &pod.name, rng.gen::<u128>());
     //let config = read_yaml_config(stdin);
     //println!("{:?}", config);
 
-    let pod_sandbox_config = m.pod_sandbox_config(&String::from(NAME), &uid);
+    let pod_sandbox_config = m.pod_sandbox_config(&pod.name, &uid);
     let pod_sandbox_id = m.run_pod_sandbox(&pod_sandbox_config).await?;
-    let image_ref = m
-        .pull_image(&IMAGE.to_string(), &pod_sandbox_config)
-        .await?;
-    let container = m
-        .create_container(
-            &IMAGE.to_string(),
-            &CONTAINER_NAME.to_string(),
-            &pod_sandbox_id,
-            &pod_sandbox_config,
-        )
-        .await?;
 
-    m.start_container(&container).await?;
+    let mut container_ids = vec![];
+    for container in pod.containers {
+        // Per container config
+        let image_ref = m.pull_image(&container.image, &pod_sandbox_config).await?;
+        let container_id = m
+            .create_container(
+                // TODO: see if we can replace with image_ref
+                &container.image,
+                &container.name,
+                &pod_sandbox_id,
+                &pod_sandbox_config,
+            )
+            .await?;
+        container_ids.push(container_id)
+    }
 
-    loop {
+    for container_id in &container_ids {
+        m.start_container(&container_id).await?;
+    }
+
+    'outer: loop {
         match term.load(Ordering::Relaxed) {
             0 => {
-                match m.poll_container_status(&container).await? {
-                    ContainerState::ContainerExited => {
-                        info!("container exited");
-                        break;
+                for container_id in &container_ids {
+                    match m.poll_container_status(&container_id).await? {
+                        ContainerState::ContainerExited => {
+                            info!("container exited");
+                            break 'outer;
+                        }
+                        _ => (),
                     }
-                    _ => (),
                 }
                 std::thread::sleep(std::time::Duration::from_millis(1000));
             }
